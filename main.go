99e1a1025e96152964e781c48815e5b1a7bc9cfd
user <vujko007@gmail.com>
2020-05-23 17:34:24 +0300

    package doc comments

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+/*
+	In the main file we collect all function that needed for running our app.
+*/
+
 func main() {
 	println("Victory!")
 }
---
+++ model/user.go
@@ -0,0 +1,7 @@
+package model
+
+/*
+	In this file, the model that we will often use will be described. Such models as User, World, Weapons, etc.
+	These models will be used when writing, reading in the database, and also when receiving data from the client,
+	in our case, from the bot.
+*/
