7be2f7482063e14d2233d2d8babb1a4f9344fdb7
Milkadpua <65362303+Milkadpua@users.noreply.github.com>
2020-06-04 11:38:54 +0300

    Update settings.go

--- config/settings.go
+++ config/settings.go
@@ -1,8 +1,8 @@
 package conf
 
 const (
-	BOT_TOKEN = "" //Your bot token
-	//TELEGRAM_URL = "https://api.telegram.org/bot"
+	BOT_TOKEN = "1096824996:AAE-YL-XR97B6D1trRl06fSgGNX6H7gs1VE" 
 	BOT_PORT = "8090"
-	WEB_HOOK = "" //Ngrok url
+	WEB_HOOK = "https://bc25db00211a.ngrok.io " //Ngrok url
+	
 )
