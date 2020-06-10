562c04f4525fc0bda8dbd8ec4464d83959e7a7a3
Arkziz <65362518+Arkziz@users.noreply.github.com>
2020-06-10 14:39:10 +0300

    Update user.go

--- model/user.go
+++ model/user.go
@@ -19,4 +19,5 @@ type User struct {
 	Defence   float64        `json:"defence"`
 	Intellect float64        `json:"intellect"`
 	Level     float64        `json:"level"`
+	Currency  int 			`json:"currency"`
 }
